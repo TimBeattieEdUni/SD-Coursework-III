02bc0a2ba7f47c87242719d15a8020b279fe97fa
Tim Beattie <tim@timthemighty.com>
2015-03-26 10:53:07 +0000

    Improved readability in main().

--- src/tsp.py
+++ src/tsp.py
@@ -228,7 +228,7 @@ import traceback
 def PrintUsage():
     print("usage: " + sys.argv[0] + " <cities> <citiesfile> <solutionfile> [<seed>]")
     print("where:")
-    print("    <cities>        is the number of cities to visit.")
+    print("    <cities>        is the number of cities to visit (must be 3 or greater).")
     print("    <citiesfile>    is a \"pickle\" file containing cities and distances.")
     print("    <solutionfile>  is the file to write the solution to.")
     print("    <seed>          is an optional seed for the random number generator.")
@@ -240,6 +240,12 @@ def main(argv):
         PrintUsage()
         sys.exit(-1)
     
+    # check number of cities is sane: a number, 3 or more
+    num_cities = int(argv[0])
+    if (3 > num_cities):
+        print("error: invalid number of cities")
+        sys.exit(-1)
+    
     # check input file exists
     infilename = argv[1]
     try:
@@ -248,7 +254,7 @@ def main(argv):
         print("error: unable to open input file " + infilename)
         sys.exit(-1)
 
-	# check output file can be opened
+    # check output file can be opened
     outfilename = argv[2]
     try:
         outfile = open(outfilename, 'w+')
@@ -261,58 +267,62 @@ def main(argv):
         seed = argv[3]
         print("seeding random number generator with value " + seed)
         random.seed(seed)
-        
-    nm = 10
-    if len(argv) >= 3 and argv[0]:
-        nm = int(argv[0])
 
-    if nm <= 10:
-        na = 20
-        ni = 12
-        nr = 1
+    # configuration
+    # leaving these magic numbers in place for now until they're understood better
+    
+    if num_cities <= 10:
+        num_ants = 20
+        num_iters = 12
     else:
-        na = 28
-        ni = 20
-        nr = 1
+        num_ants = 28
+        num_iters = 20
 
-	# load cities and distances
-    stuff = pickle.load(infile)
-    cities = stuff[0]
-    cm = stuff[1]
-    #why are we doing this?
-    if nm < len(cm):
-        cm = cm[0:nm]
-        for i in range(0, nm):
-            cm[i] = cm[i][0:nm]
+    # load cities and distances
+    city_data = pickle.load(infile)
+    cities = city_data[0]
+    city_matrix = city_data[1]
 
-
+    # truncate the city matrix to just those cities we'll be visiting
+    if num_cities < len(city_matrix):
+        city_matrix = city_matrix[0:num_cities]
+        for i in range(0, num_cities):
+            city_matrix[i] = city_matrix[i][0:num_cities]
 
     try:
-        graph = GraphBit(nm, cm)
-        bpv = None
-        bpc = sys.maxint
-        for i in range(0, nr):
-            print "Repetition %s" % i
-            graph.reset_tau()
-            workers = BigGroup(graph, na, ni)
-            print "Colony Started"
-            workers.start()
-            if workers.bpc < bpc:
-                print "Colony Path"
-                bpv = workers.bpv
-                bpc = workers.bpc
+        # set up data structure and colony
+        graph = GraphBit(num_cities, city_matrix)
+        best_path_indices = None
+        best_distance = sys.maxint
 
+        graph.reset_tau()
+        workers = BigGroup(graph, num_ants, num_iters)
+        
+        print "Colony Started"
+
+        # run the calculation
+        workers.start()
+        if workers.bpc < best_distance:
+            print "Colony Path"
+            best_path_indices = workers.bpv
+            best_distance = workers.bpc
+
+        # print results
         print "\n------------------------------------------------------------"
         print "                     Results                                "
         print "------------------------------------------------------------"
-        print "\nBest path = %s" % (bpv,)
-        city_vec = []
-        for node in bpv:
-            print cities[node] + " ",
-            city_vec.append(cities[node])
-        print "\nBest path cost = %s\n" % (bpc,)
-        results = [bpv, city_vec, bpc]
+        print "\nBest path = %s" % (best_path_indices,)
+
+        best_path = []
+        for index in best_path_indices:
+            print cities[index] + " ",
+            best_path.append(cities[index])
+
+        print "\nBest path distance = %s\n" % (best_distance,)
+        results = [best_path_indices, best_path, best_distance]
         pickle.dump(results, outfile)
+
+    # current approach to error handling is to report the error and give up
     except Exception, e:
         print "exception: " + str(e)
         traceback.print_exc()
