cdb1033426a72a9ea080c424d3b5519d2a064864
Tim Beattie <tim@timthemighty.com>
2015-03-27 00:00:54 +0000

    Improved readability in class GraphBit.

--- src/tsp.py
+++ src/tsp.py
@@ -171,31 +171,36 @@ class BigGroup:
                     self.graph.update_tau(r, s, evaporation + deposition)
 
 class GraphBit:
-    def __init__(self, num_nodes, delta_mat, tau_mat=None):
-        print len(delta_mat)
-        if len(delta_mat) != num_nodes:
-            raise Exception("len(delta) != num_nodes")
-        self.num_nodes = num_nodes
+    """Stores delta and tau matrices and provides operations on matrix elements."""
+
+    def __init__(self, delta_mat, tau_mat=None):
+        """Stores the given matrices (generating zeroes in tau matrix if not provided.)"""
+        self.num_nodes = len(delta_mat)
         self.delta_mat = delta_mat 
         if tau_mat is None:
             self.tau_mat = []
-            for i in range(0, num_nodes):
-                self.tau_mat.append([0] * num_nodes)
+            for i in range(0, self.num_nodes):
+                self.tau_mat.append([0] * self.num_nodes)
 
     def delta(self, r, s):
+        """Element access for delta matrix."""
         return self.delta_mat[r][s]
 
     def tau(self, r, s):
+        """Element access for tau matrix."""
         return self.tau_mat[r][s]
 
     def etha(self, r, s):
+        """Returns reciprocal of element in delta matrix.  Not yet clear what this is for - part of the ant colony algorithm."""
         return 1.0 / self.delta(r, s)
 
     def update_tau(self, r, s, val):
+        """Sets the given element in the tau matrix."""
         self.tau_mat[r][s] = val
 
     def reset_tau(self):
-        avg = self.average_delta()
+        """Calculates a value and assigns every element of the tau matrix that value.  Not yet clear how the value is chosen."""
+        avg = self.average(self.delta_mat)
         self.tau0 = 1.0 / (self.num_nodes * 0.5 * avg)
         print "Average = %s" % (avg,)
         print "Tau0 = %s" % (self.tau0)
@@ -203,15 +208,8 @@ class GraphBit:
             for s in range(0, self.num_nodes):
                 self.tau_mat[r][s] = self.tau0
 
-
-    def average_delta(self):
-        return self.average(self.delta_mat)
-
-
-    def average_tau(self):
-        return self.average(self.tau_mat)
-
     def average(self, matrix):
+        """Returns the average of all values in the given matrix."""
         sum = 0
         for r in range(0, self.num_nodes):
             for s in range(0, self.num_nodes):
@@ -291,7 +289,7 @@ def main(argv):
 
     try:
         # set up data structure and colony
-        graph = GraphBit(num_cities, city_matrix)
+        graph = GraphBit(city_matrix)
         best_path_indices = None
         best_distance = sys.maxint
 
@@ -322,7 +320,7 @@ def main(argv):
         results = [best_path_indices, best_path, best_distance]
         pickle.dump(results, outfile)
 
-    # current approach to error handling is to report the error and give up
+    # current approach to error handling is to report any error and give up
     except Exception, e:
         print "exception: " + str(e)
         traceback.print_exc()
